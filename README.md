02367e4948d54dd38d087e4fe210af5ae816a231
caowen <caowen@oneapm.com>
2016-03-25 22:30:07 +0800

    new project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# graduation-project1
+# graduation-project
+It's about O2O e-business in domestic service
+##
+###how to install
+####<code>git clone git@github.com:September-9/graduation-project.git</code>
+and code <code>npm install</code> to get start. 
+###how to start
+enter <code>node start</code> to start the server and enter "localhost:8000" to open index page.
---
+++ app.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var test = require('./routes/test');
+var users = require('./routes/users');
+
+var app = express();
+
+app.use(express.static('public'));
+
+var swig = require('swig'),
+  people;
+
+app.set('view engine','html');
+app.engine('html',swig.renderFile);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/test', test);
+app.use('/users', users);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+app.use(function(err,req,res,next){
+  res.status(404).send('Sorry cannot find that!');
+})
+
+app.use(function(err,req,res,next){
+  res.status(500).send('Something broke!');
+})
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+var server = app.listen(8000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
+module.exports = app;
