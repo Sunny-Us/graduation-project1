620c0ed9863f50a4ae888ad76efdd9a61ec7d9f2
caowen <caowen@oneapm.com>
2016-03-27 21:28:21 +0800

    service detail

--- routes/service_detail.js
+++ routes/service_detail.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('server_detail');
+  res.render('service_detail');
 });
 
 module.exports = router;
